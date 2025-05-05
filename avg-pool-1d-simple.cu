3935bd9229e2f43a7d0ba3152997edede33a40e0
CyrilSharma <sharma.cyril@gmail.com>
2025-05-05 11:54:41 -0400

    Added some files

---
+++ avg-pool-1d-simple.cu
@@ -0,0 +1,74 @@
+#include "solve.h"
+#include <cstdio>
+#include <cuda_runtime.h>
+
+// This is an excellent algorithm for the wrong problem.
+// I solved the sliding window problem instead of the convolution problem...
+__global__ void convolution_1d_kernel(const float *input, float *partialSums,
+                                      float *output, int I, int K, int B) {
+  // We assume effectively infinite threads. Depth is the amount of work each
+  // thread would have to do. Naive: NK work, depth of K Chunk array by K: N
+  // work, depth of K Prefix: N work, depth of log(N)... but accuracy issues,
+  // and hard to implement. Chunk array by C: N + (K / C)(N / C) work, depth of
+  // max(C, K / C) Under the constraints, we always have enough threads. Can
+  // just choose C = sqrt(K) Chunk array by sqrt(K): N work, depth of sqrt(K)
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  int nbuckets = (I + B - 1) / B;
+  if (id >= nbuckets)
+    return;
+
+  int bstart = (id * B);
+  int bend = min(bstart + B - 1, I - 1);
+  for (int i = bstart; i <= bend; ++i)
+    partialSums[id] += input[i];
+  __syncthreads();
+
+  if (id * B >= (I - K + 1))
+    return;
+
+  // sum of input[bstart:bstart+K]
+  // partialSums[i] is the sum of input[i*B:i*(B+1)]
+  int sum = 0;
+  int cur = bstart;
+  int dest = bstart + K;
+  while (cur < dest) {
+    if ((dest - cur < B) || (cur % B != 0)) {
+      sum += input[cur];
+      cur += 1;
+    } else {
+      sum += partialSums[cur / B];
+      cur += B;
+    }
+  }
+
+  // output[i] is the sum of input[i:i+K]
+  int nend = min(bend, I + K - 1);
+  for (int i = bstart; i <= nend; ++i) {
+    output[i] = sum;
+    sum -= input[i];
+    sum += input[i + K - 1];
+  }
+}
+
+// input, kernel, output are device pointers (i.e. pointers to memory on the
+// GPU)
+void solve(const float *input, float *output, int input_size, int kernel_size) {
+  int output_size = input_size - kernel_size + 1;
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (output_size + threadsPerBlock - 1) / threadsPerBlock;
+
+  int B = min(kernel_size, 30);
+  float *partialSums;
+  size_t partialSize = (input_size + B - 1) / B;
+  cudaMalloc((void **)&partialSums, partialSize * sizeof(float));
+
+  convolution_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(
+      input, partialSums, output, input_size, kernel_size, B);
+  cudaDeviceSynchronize();
+
+  float *h_partialSums = (float *)malloc(partialSize * sizeof(float));
+  cudaMemcpy(h_partialSums, partialSums, partialSize * sizeof(float),
+             cudaMemcpyDeviceToHost);
+  for (int i = 0; i < partialSize; ++i)
+    printf("p[%d] = %f\n", i, h_partialSums[i]);
+}
--- avg-pool-1d.cu
+++ avg-pool-1d.cu
@@ -95,7 +95,7 @@ void solution3(const float *input, int kernel_size, int stride, int padding,
   cudaMemcpy(d_input, input, H * sizeof(float), cudaMemcpyHostToDevice);
 
   // 3) Launch kernel
-  solution2Kernel<<<1, 512>>>(d_input, kernel_size, stride, padding, d_output,
+  solution3Kernel<<<1, 512>>>(d_input, kernel_size, stride, padding, d_output,
                               H, HOUT);
 
   // 4) Synchronize & check errors
@@ -109,20 +109,143 @@ void solution3(const float *input, int kernel_size, int stride, int padding,
   cudaFree(d_output);
 }
 
+__global__ void solution4Kernel(const float *input, int kernel_size, int stride,
+                                int padding, float *output, size_t H,
+                                int H_OUT) {
+  extern __shared__ float tile[];
+
+  bool overlap = true; // (stride < kernel_size);
+  int tileSize = (blockDim.x * stride) + (kernel_size - 1);
+  int outPos = blockDim.x * blockIdx.x;
+  int inPos = (outPos * stride) - padding;
+  if (overlap) {
+    for (int i = threadIdx.x; i < tileSize; i += blockDim.x) {
+      int curPos = inPos + i;
+      tile[i] = (curPos >= 0 && curPos < H) ? input[curPos] : 0.0f;
+    }
+  }
+  // TODO: pack things contiguously if the stride is larger then the kernel.
+  __syncthreads();
+
+  float sum = 0.0;
+  int pos = threadIdx.x * (overlap ? (stride) : (kernel_size));
+  for (int j = 0; j < kernel_size; ++j) {
+    if (pos + j >= 0 && pos + j < H) {
+      sum += tile[pos + j];
+    }
+  }
+  output[blockDim.x * blockIdx.x + threadIdx.x] = (sum / kernel_size);
+}
+
+void solution4(const float *input, int kernel_size, int stride, int padding,
+               float *output, size_t H) {
+  const int HSIGNED = (int)(H);
+  const int HOUT = ((HSIGNED + 2 * padding - kernel_size) / stride) + 1;
+  const int threadsPerBlock = 512;
+  const int numBlocks = (HOUT + threadsPerBlock - 1) / threadsPerBlock;
+
+  // This is great if the stride is small relative to the kernel.
+  // It does a lot of extra work if the stride is large relative to the kernel.
+  const int sharedBytes =
+      (threadsPerBlock * stride + kernel_size - 1) * sizeof(float);
+
+  float *d_input = nullptr, *d_output = nullptr;
+  cudaMalloc(&d_input, H * sizeof(float));
+  cudaMalloc(&d_output, HOUT * sizeof(float));
+
+  cudaMemcpy(d_input, input, H * sizeof(float), cudaMemcpyHostToDevice);
+
+  // 3) Launch kernel
+  solution4Kernel<<<numBlocks, threadsPerBlock, sharedBytes>>>(
+      d_input, kernel_size, stride, padding, d_output, H, HOUT);
+
+  // 4) Synchronize & check errors
+  cudaError_t err = cudaDeviceSynchronize();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "CUDA error after kernel: %s\n", cudaGetErrorString(err));
+  }
+
+  cudaMemcpy(output, d_output, HOUT * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaFree(d_input);
+  cudaFree(d_output);
+}
+
+#define padGet(A, sz, x) ((x) < (sz)) ? (A[(x)]) : 0
+__global__ void solution5Kernel(const float *input, int kernel_size, int stride,
+                                int padding, float *output, size_t H,
+                                int H_OUT) {
+  int slots_owned = kernel_size;
+  int outputPos = ((blockIdx.x * blockDim.x) + threadIdx.x) * slots_owned;
+  int inputPos = (outputPos * stride) - padding;
+  if (outputPos >= H_OUT)
+    return;
+
+  float sum = 0.0;
+  for (int i = 0; i < kernel_size; ++i) {
+    sum += padGet(input, H, (inputPos + i));
+  }
+  output[outputPos] = sum / kernel_size;
+
+  int cnt = 0;
+  for (int i = 1; i < slots_owned; ++i) {
+    if (outputPos + i >= H_OUT)
+      return;
+    for (int j = 0; j < stride; ++j) {
+      sum += padGet(input, H, inputPos + kernel_size + cnt);
+      sum -= padGet(input, H, inputPos + cnt);
+      ++cnt;
+    }
+    output[outputPos + i] = sum / kernel_size;
+  }
+}
+
+void solution5(const float *input, int kernel_size, int stride, int padding,
+               float *output, size_t H) {
+  const int HSIGNED = (int)(H);
+  const int HOUT = ((HSIGNED + 2 * padding - kernel_size) / stride) + 1;
+  const int threadsPerBlock = 1024;
+  const int outputsPerBlock = threadsPerBlock * kernel_size;
+  const int numBlocks = (HOUT + outputsPerBlock - 1) / outputsPerBlock;
+  float *d_input = nullptr, *d_output = nullptr;
+
+  cudaMalloc(&d_input, H * sizeof(float));
+  cudaMalloc(&d_output, HOUT * sizeof(float));
+  cudaMemcpy(d_input, input, H * sizeof(float), cudaMemcpyHostToDevice);
+
+  // 3) Launch kernel
+  solution5Kernel<<<numBlocks, threadsPerBlock>>>(d_input, kernel_size, stride,
+                                                  padding, d_output, H, HOUT);
+
+  // 4) Synchronize & check errors
+  cudaError_t err = cudaDeviceSynchronize();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "CUDA error after kernel: %s\n", cudaGetErrorString(err));
+  }
+
+  cudaMemcpy(output, d_output, HOUT * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaFree(d_input);
+  cudaFree(d_output);
+}
+
+template <typename T> void printVector(vector<T> &a) {
+  for (auto el : a)
+    std::cout << el << " ";
+  std::cout << '\n';
+}
+
 // Note: input, output are all device pointers to float32 arrays
 int main() {
-  int N = 5000;
-  vector<float> A(N), B(N + 1);
+  int N = 5;
+  const int kernel_size = 2;
+  const int stride = 2;
+  const int padding = 1;
+  const int M = ((N + 2 * padding - kernel_size) / stride) + 1;
+  vector<float> A(N), B(M);
   for (int i = 0; i < N; ++i) {
     A[i] += 2 * i;
   }
 
-  const int kernel_size = 2;
-  const int stride = 1;
-  const int padding = 1;
-  solution2(A.data(), kernel_size, stride, padding, B.data(), A.size());
-  for (auto el : B) {
-    std::cout << el << " ";
-  }
-  std::cout << '\n';
+  solution5(A.data(), kernel_size, stride, padding, B.data(), A.size());
+  printVector(A);
+  printVector(B);
 }
